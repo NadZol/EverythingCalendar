681369f051323ec8c228a231385062db7dcb05e3
NadZol <nadja.zolotareva@gmail.com>
2020-06-03 23:04:21 -0400

    Added ability to switch between fragments. Clicking on 'Add/New' option crashes the app.

---
+++ app/src/main/java/com/example/everythingcalendar/AddNewFragment.java
@@ -0,0 +1,4 @@
+package com.example.everythingcalendar;
+
+public class AddNewFragment {
+}
--- app/src/main/java/com/example/everythingcalendar/MainActivity.java
+++ app/src/main/java/com/example/everythingcalendar/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.everythingcalendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch(item.getItemId()) {
+                case R.id.nav_notes:
+                    fragment = new NotesFragment();
+                    break;
+                case R.id.nav_to_do_reminder:
+                    fragment = new ToDoReminderFragment();
+                    break;
+                case R.id.nav_calendar:
+                    fragment = new CalendarFragment();
+                    break;
+                case R.id.nav_settings:
+                    fragment = new SettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+            return true;
+        }
+    };
 }
