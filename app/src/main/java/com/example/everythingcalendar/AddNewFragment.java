34610725f2e9918f40f2574384c0ef8f5edbac7c
NadZol <nadja.zolotareva@gmail.com>
2020-06-05 20:41:06 -0400

    Removed Add/New tab on the bottom navigation view, added note edit view.

--- app/src/main/java/com/example/everythingcalendar/AddNewFragment.java
+++
@@ -1,4 +0,0 @@
-package com.example.everythingcalendar;
-
-public class AddNewFragment {
-}
--- app/src/main/java/com/example/everythingcalendar/NotesFragment.java
+++ app/src/main/java/com/example/everythingcalendar/NotesFragment.java
@@ -1,9 +1,15 @@
 package com.example.everythingcalendar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +17,26 @@ import androidx.fragment.app.Fragment;
 
 public class NotesFragment extends Fragment {
 
+    ListView listView;
+    String[] notes;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        listView = (ListView)view.findViewById(R.id.listView);
+        notes = new String[]{"example note", "example note 1"};
+        ArrayAdapter<String> notesAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(notesAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
